7736e9d58dc0df4235b8d069d8a1ce5fd9acf35e
SmallSun697 <smallsun698@gmail.com>
2024-10-27 01:03:56 +0800

    add:Automatically update local repositories(app.js)

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const {join} = require("path");
+const { join } = require('path');
+const { execSync } = require('child_process');
+const cron = require('node-cron');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+let fetch;
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
@@ -13,4 +18,16 @@ app.get('/time', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
+});
+
+cron.schedule('*/10 * * * * *', () => {
+    fetch(""/*TODO*/, {
+        headers: { "branchUpdateTime": ""/*TODO*/ }
+    })
+        .then(response => response.text())
+        .then(data => {
+            if (data === "true") {
+                execSync("git pull https://github.com/SmallSun697/ExamCountdown");
+            }
+        });
 });
