999ad4f97941e374bfd1271c1f535d723647da1f
SmallSun697 <smallsun698@gmail.com>
2024-10-26 14:51:12 +0800

    add:Server settings(app.ts, node.js)

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {join} = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
