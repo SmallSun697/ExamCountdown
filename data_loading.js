3825e2e56b33e2b33f6fefecd6168cfec2d1ebcb
smallsun697 <smallsun698@gmail.com>
2024-10-30 16:20:56 +0800

    add:Dynamic page loading(data_loading.js, index.html)

--- data_loading.js
+++ data_loading.js
@@ -7,27 +7,38 @@ function updateDatabase() {
         .then(response => response.text())
         .then(data => {
             if (data === "true") {
-                location.href = '/';
+                location.reload();
             }
         });
 }
 
 function loadPage(page) {
+    const old = document.body.querySelectorAll(".display");
     const iframe = document.createElement("iframe");
     if (window.location.hash) {
         page = window.location.hash.slice(1);
-    }else if (!page) {
+    } else if (!page) {
         //page = /*TODO:database*/
     }
+    old.forEach(item => {
+        item.classList.add("old");
+    });
     iframe.src = `${page}.html`;
+    iframe.classList.add("display");
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.style.border = "none";
     iframe.style.position = "fixed";
     iframe.style.top = "0";
     iframe.style.left = "0";
-    document.body.innerHTML = '';
+    iframe.style.display = "none";
     document.body.appendChild(iframe);
+    iframe.onload = () => {
+        document.querySelectorAll(".old").forEach((item) => {
+            item.remove();
+        })
+        iframe.style.display = "block";
+    }
 }
 
 (async () => {
