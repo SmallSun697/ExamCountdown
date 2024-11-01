ea80c956c1e87619f1ec71b78d0172bcb4054f5d
smallsun697 <smallsun698@gmail.com>
2024-11-01 19:18:42 +0800

    add:Cursor hidden(script.js) chron:Restructure project file organization(dynamic_color.js => script.js)

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    const hash = window.parent.location.hash.slice(1);
+    let mainColor, secondaryColor;
+    if (hash) {
+        [mainColor, secondaryColor] = hash.split('&').slice(-2);
+    } else {
+        [mainColor, secondaryColor] = [];/*TODO: API database theme*/
+    }
+    document.documentElement.style.setProperty("--main-color", `#${mainColor}`);
+    document.documentElement.style.setProperty("--secondary-color", `#${secondaryColor}`);
+}
+
+document.addEventListener("mousemove", () => {
+    document.body.style.cursor = "default";
+});
+
+document.addEventListener("keydown", (Event) => {
+    if (Event.key === " ") {
+        document.body.style.cursor = "none";
+    }
+});
